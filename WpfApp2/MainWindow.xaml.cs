33758c65b849a0c0209fce651ccb227a5769b905
Anton S <geodesist.sushkov@gmail.com>
2022-03-04 16:48:15 +0300

    добавлен фунционал

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -21,9 +21,97 @@ namespace WpfApp2
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        double number1 = 0;
+        public string Ariph;
+        bool flagN2Active;
+
         public MainWindow()
         {
+            flagN2Active = false;
             InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (flagN2Active == true)
+            {
+                flagN2Active = false;
+                textBlock.Text = "0";
+            }
+            Button B = (Button)sender;
+            if (textBlock.Text == "0")
+                textBlock.Text = (string)B.Content;
+            else
+            textBlock.Text =textBlock.Text + B.Content;
+        }
+
+
+        private void ButtonС_Click(object sender, RoutedEventArgs e)
+        {
+            textBlock.Text = "0";
+        }
+
+        private void ButtonAriph_Click(object sender, RoutedEventArgs e)
+        {
+            Button B = (Button)sender;
+            Ariph = (string)B.Content;
+            number1 =Convert.ToDouble(textBlock.Text);
+            flagN2Active = true;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double N1, N2;
+            double result = 0;
+            N1 = number1;
+            N2 = Convert.ToDouble(textBlock.Text);
+            if(Ariph == "+")
+            {
+                result = N1 + N2;
+            }
+            if (Ariph == "-")
+            {
+                result = N1 - N2;
+            }
+            if (Ariph == "*")
+            {
+                result = N1 * N2;
+            }
+            if (Ariph == "/")
+            {
+                result = N1 / N2;
+            }
+            if (Ariph == "%")
+            {
+                result = N1 * N2/100;
+            }
+            Ariph = "=";
+            flagN2Active = true;
+            textBlock.Text = result.ToString();
+        }
+
+        private void ButtonSqr_Click(object sender, RoutedEventArgs e)
+        {
+            double result;
+            number1 = Convert.ToDouble(textBlock.Text);
+            result = Math.Sqrt(number1);
+            textBlock.Text = result.ToString();
+        }
+
+        private void ButtonPow_Click(object sender, RoutedEventArgs e)
+        {
+            double result;
+            number1 = Convert.ToDouble(textBlock.Text);
+            result = Math.Pow(number1,2);
+            textBlock.Text = result.ToString();
+        }
+
+
+        private void ButtonPoint_Click(object sender, RoutedEventArgs e)
+        {
+            if(!textBlock.Text.Contains("."))
+            textBlock.Text = textBlock.Text + ".";
         }
     }
 }
